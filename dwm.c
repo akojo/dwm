606561b3fe47a16ac02223a5792f44f1b1a21b88
Atte Kojo <atte.kojo@gmail.com>
2020-05-18 22:08:16 +0300

    Custom fullscreen mode
    - Don't toggle statusbar in fullscreen mode
    - When running fullscreen(), toggle between tiled and monocle layouts
    - Disable layout toggling when clicking layout indicator
    - Use Mod+Space to toggle fullscreen mode

--- dwm.c
+++ dwm.c
@@ -1655,17 +1655,14 @@ setfullscreen(Client *c, int fullscreen)
 	}
 }
 
-Layout *last_layout;
 void
 fullscreen(const Arg *arg)
 {
-	if (selmon->showbar) {
-		for(last_layout = (Layout *)layouts; last_layout != selmon->lt[selmon->sellt]; last_layout++);
+	if (selmon->lt[selmon->sellt] == &layouts[0]) {
 		setlayout(&((Arg) { .v = &layouts[2] }));
 	} else {
-		setlayout(&((Arg) { .v = last_layout }));
+		setlayout(&((Arg) { .v = &layouts[0] }));
 	}
-	togglebar(arg);
 }
 
 void
