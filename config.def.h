ebdb3e729e93696f928e0c6b5b77a27b106577ac
Atte Kojo <atte.kojo@gmail.com>
2020-05-18 20:39:56 +0300

    Custom configuration
    - Use Mod4 (windows) as modifier key
    - Add functions for laptop media keys
    - Disable other than tiled layouts
    - Use solarized colors, also for dmenu
    - Put dmenu in vertical orientation
    - Use larger font for dmenu

--- config.def.h
+++ config.def.h
@@ -1,4 +1,5 @@
 /* See LICENSE file for copyright and license details. */
+#include <X11/XF86keysym.h>
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
@@ -10,16 +11,34 @@ static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char dmenufont[]       = "monospace:size=14";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+/* Solarized colors */
+static const char sol_base03[] = "#002b36";
+static const char sol_base02[] = "#073642";
+static const char sol_base01[] = "#586e75";
+static const char sol_base00[] = "#657b83";
+static const char sol_base0[] = "#839496";
+static const char sol_base1[] = "#93a1a1";
+static const char sol_base2[] = "#eee8d5";
+static const char sol_base3[] = "#fdf6e3";
+
+static const char sol_yellow[] = "#b58900";
+static const char sol_orange[] = "#cb4b16";
+static const char sol_red[] = "#dc322f";
+static const char sol_magenta[] = "#d33682";
+static const char sol_violet[] = "#6c71c4";
+static const char sol_blue[] = "#268bd2";
+static const char sol_cyan[] = "#2aa198";
+static const char sol_green[] = "#859900";
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	/*               fg         bg          border   */
+	[SchemeNorm] = { sol_base0, sol_base03, sol_base02 },
+	[SchemeSel]  = { sol_base1, sol_base02, sol_orange  },
 };
 
 /* tagging */
@@ -48,7 +67,7 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -60,8 +79,15 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-l", "20", "-m", dmenumon, "-fn", dmenufont, "-nb", sol_base03, "-nf", sol_base0, "-sb", sol_base02, "-sf", sol_green, NULL };
+static const char *termcmd[] = { "st", NULL };
+
+static const char *upvol[] = {"amixer", "set", "Master", "2dB+", NULL};
+static const char *downvol[] = {"amixer", "set", "Master", "2dB-", NULL};
+static const char *mutevol[] = {"amixer", "-Dpulse", "set", "Master", "toggle", NULL};
+static const char *brightnessup[] = {"xbacklight", "-inc", "2", NULL};
+static const char *brightnessdown[] = {"xbacklight", "-dec", "2", NULL};
+static const char *displays[] = {"arandr", NULL};
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -95,6 +121,12 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{0,              XF86XK_AudioRaiseVolume,  spawn,          {.v = upvol}},
+	{0,              XF86XK_AudioLowerVolume,  spawn,          {.v = downvol}},
+	{0,              XF86XK_AudioMute,         spawn,          {.v = mutevol}},
+	{0,              XF86XK_MonBrightnessUp,   spawn,          {.v = brightnessup}},
+	{0,              XF86XK_MonBrightnessDown, spawn,          {.v = brightnessdown}},
+	{0,              XF86XK_Display,           spawn,          {.v = displays}},
 };
 
 /* button definitions */
